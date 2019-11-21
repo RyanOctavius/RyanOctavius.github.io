509efc3c99567e080a374cd77eecdc81d5de1a90
RyanOctavius <ryanoctavius8899@gmail.com>
2019-11-21 12:11:49 +0700

    ugd pwa vcard

---
+++ precache-manifest.00ce3f1aa3de8482fb303350622fd69f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff938837316e8bfd1b60",
+    "url": "/css/chunk-vendors.7b32bd5a.css"
+  },
+  {
+    "revision": "2c2aee49c3ce783f3ce1d7831cc22a7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f69fc72edf8dbad43c3",
+    "url": "/js/about.af5b8428.js"
+  },
+  {
+    "revision": "8ef6c22cb47c50d245f2",
+    "url": "/js/app.b4524058.js"
+  },
+  {
+    "revision": "ff938837316e8bfd1b60",
+    "url": "/js/chunk-vendors.1f26f6d6.js"
+  },
+  {
+    "revision": "2bba09e32a4be3ea807e156a88c0866e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
